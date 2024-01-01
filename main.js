ed2bbaa22954c5fed040ad9e7715a5990df5a733
gabriel <gf577030@gmail.com>
2024-01-01 11:09:14 -0300

    arquivo  js

---
+++ main.js
@@ -0,0 +1,30 @@
+ // var nome = 'gabriel'; //
+// este é meu primeiro código em ja
+
+
+
+
+
+function calculo() {
+
+
+var pergunta= prompt('quem descobriu o brasil');
+var reposta = 'Pedro Cabral'
+
+if(pergunta === reposta){
+    alert('parabéns vc acertou')
+}
+
+else
+alert('voce errou')
+
+}
+
+
+
+
+
+
+
+
+
---
+++ main2.js
@@ -0,0 +1,7 @@
+console.log('ola mundo') //tipo o print//
+
+
+
+
+
+  
